8d0e18c0bd673a33b06be6bf5b9429935cbd6da3
anouar <tamagoult.anouar@gmail.com>
2024-11-17 22:27:54 +0100

    1.0.2

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
   entry: './src/Modal.jsx',
@@ -16,10 +17,15 @@ module.exports = {
       },
       {
         test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
+        use: [MiniCssExtractPlugin.loader, 'css-loader'], // Remplace style-loader par MiniCssExtractPlugin
       },
     ],
   },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'modal.css', // Génère un fichier modal.css dans dist/
+    }),
+  ],
   externals: {
     react: 'react',
     'react-dom': 'react-dom',
